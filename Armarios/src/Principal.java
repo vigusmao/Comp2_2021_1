7a6e9b351dc3d6560dd4d2b8a92ed184866e6b77
vigusmao <vigusmao@gmail.com>
2021-07-14 09:27:17 -0300

    crescimento geometrico de arrays

--- Armarios/src/Principal.java
+++ Armarios/src/Principal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Principal {
 
-    public static int obterArmariosAbertosViaSimulacao(int n) {
+    public static int[] obterArmariosAbertosViaSimulacao(int n) {
         /* false: armário fechado;
            true: armário aberto */
         boolean[] armarios ;  // é null neste momento
@@ -11,25 +11,78 @@ public class Principal {
         // O array contém apenas false neste momento (armários todos fechados)
 
         for (int crianca = 1; crianca <= n; crianca++) {
-            for (int armario = crianca; armario <= n; armario += crianca) {
-                armarios[armario] = !armarios[armario];
+            for (int armario = 1; armario <= n; armario++) {
+                if (armario % crianca == 0) {
+                    armarios[armario] = !armarios[armario];
+                }
             }
         }
+
+        int[] armariosAbertos = new int[10];
+        /*
+                 armariosAbertos --------> |||  $0FC6A6  |||
+                                                    {1}        {4}
+         */
+
 
         int contAbertos = 0;
         for (int armario = 1; armario <= n; armario++) {
             if (armarios[armario]) {
+
+                if (contAbertos >= armariosAbertos.length) {  // OVERFLOW!!!!
+
+                    // cria um novo array
+                    int[] novoArray = new int[armariosAbertos.length * 2];
+
+                    /*
+                         novoArray   --------> |||  $43210B  |||
+                                                        {1}      {4}    { }     { }
+                    */
+                    // copia o array corrente inteiro para o novo array
+                    for (int i = 0; i < armariosAbertos.length; i++) {
+                        novoArray[i] = armariosAbertos[i];
+                    }
+
+                    armariosAbertos = novoArray;
+
+                    /*
+
+                       SEM REFERÊNCIAS --------> |||  $0FC6A6  |||
+                                                        {1}        {4}
+
+                       armariosAbertos --------> |||  $43210B  |||
+                       novoArray   -------->     |||  $43210B  |||
+                                                        {1}        {4}    { }     { }
+                    */
+                }
+
+                armariosAbertos[contAbertos] = armario;
                 contAbertos++;
             }
         }
 
-        return contAbertos;
+        int[] resultado = new int[contAbertos];
+        for (int i = 0; i < contAbertos; i++) {
+            resultado[i] = armariosAbertos[i];
+        }
+
+        return resultado;
     }
 
     public static int obterArmariosAbertos(int n) {
         return (int) Math.sqrt(n);
     }
 
+    public static String imprimirArray(int[] array) {
+        String resultado = "";
+        for (int i = 0; i < array.length; i++) {
+            resultado += array[i];
+            if (i < array.length - 1) {
+                resultado += ", ";
+            }
+        }
+        return resultado;
+    }
 
     public static void main(String[] args) {
         while (true) {
@@ -41,10 +94,11 @@ public class Principal {
             }
 
             long inicio = System.currentTimeMillis();
-            int contArmariosAbertos = obterArmariosAbertosViaSimulacao(contArmarios);
+            int[] armariosAbertos = obterArmariosAbertosViaSimulacao(contArmarios);
+
             long duracao = System.currentTimeMillis() - inicio;
 
-            System.out.println("Quantidade de armários abertos: " + contArmariosAbertos);
+            System.out.println("Armários abertos: " + imprimirArray(armariosAbertos));
             System.out.printf("Duração: %.3f segundos\n", duracao / 1000f);
         }
     }
