1384a8cff64c0b6bc8117a6b962516f5904ab7a0
vigusmao <vigusmao@gmail.com>
2021-08-06 09:47:31 -0300

    LAB 4 (codigo modificado)

--- Banco/src/Correntista.java
+++ Banco/src/Correntista.java
@@ -5,12 +5,12 @@ public class Correntista extends PessoaFisica {
     private static final int LIMITE_DEFAULT = 100;
 
     private float limiteChequeEspecial;
-    private ArrayList<ContaCorrente> contas;
+    private ContaCorrente contaCorrente;
     private ArrayList<AplicacaoFinanceira> investimentos;
 
     public Correntista(String nome, long cpf) {
         super(nome, cpf);
-        this.contas = new ArrayList<>();
+        this.contaCorrente = null;
         this.investimentos = new ArrayList<>();
         this.limiteChequeEspecial = LIMITE_DEFAULT;
     }
@@ -27,5 +27,7 @@ public class Correntista extends PessoaFisica {
         return 0;  // ToDo IMPLEMENT ME!!
     }
 
-
+    public ContaCorrente getContaCorrente() {
+        return contaCorrente;
+    }
 }
